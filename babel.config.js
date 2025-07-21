1ea97b9815ded312477074f074202938186318b0
Rami Elwan <ramielwan48@gmail.com>
2025-07-21 18:09:55 +0200

    chore: use unistyles

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'react-native-unistyles/plugin',
+      {
+        // pass root folder of your application
+        // all files under this folder will be processed by the Babel plugin
+        // if you need to include more folders, or customize discovery process
+        // check available babel options
+        root: 'src',
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { View, FlatList, Text, StatusBar } from 'react-native';
+
+import { StyleSheet } from 'react-native-unistyles';
+
+type TTheme = {
+  colors: {
+    'bg-primary': string;
+    'bg-secondary': string;
+    'text-primary': string;
+  };
+};
+
+type TThemes = {
+  light: TTheme;
+  dark: TTheme;
+};
+
+const light: TTheme = {
+  colors: {
+    'bg-primary': '#FFFFFF',
+    'bg-secondary': '#F5F5F5',
+    'text-primary': '#181D27',
+  },
+};
+
+const dark: TTheme = {
+  colors: {
+    'bg-primary': '#0C0E12',
+    'bg-secondary': '#22262F',
+    'text-primary': '#F7F7F7',
+  },
+};
+
+const themes: TThemes = {
+  light,
+  dark,
+};
+
+StyleSheet.configure({
+  themes,
+  settings: {
+    adaptiveThemes: true,
+  },
+});
+
+declare module 'react-native-unistyles' {
+  export interface UnistylesThemes extends TThemes { }
+}
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => (
+  <View style={styles.container}>
+    <FlatList
+      data={DATA}
+      renderItem={({ item }) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create(theme => {
+  return {
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+      backgroundColor: theme.colors['bg-primary'],
+    },
+    item: {
+      backgroundColor: theme.colors['bg-secondary'],
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      borderRadius: 4,
+    },
+    title: {
+      fontSize: 32,
+      color: theme.colors['text-primary'],
+    },
+  };
+});
+
+export default App;
