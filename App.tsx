982a8851112717d1896ca65ba5ee672d347e43be
Rami Elwan <ramielwan48@gmail.com>
2025-07-21 17:22:10 +0200

    feat: simple app

--- App.tsx
+++ App.tsx
@@ -1,27 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  SafeAreaView,
+} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const colors = {
+  dark: {
+    'bg-primary': '#0C0E12',
+    'bg-secondary': '#22262F',
+    'text-primary': '#F7F7F7',
+  },
+  light: {
+    'bg-primary': '#FFFFFF',
+    'bg-secondary': '#F5F5F5',
+    'text-primary': '#181D27',
+  },
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
-}
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <FlatList
+      data={DATA}
+      renderItem={({ item }) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
+    />
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: colors.light['bg-primary'],
+  },
+  item: {
+    backgroundColor: colors.light['bg-secondary'],
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 4,
+  },
+  title: {
+    fontSize: 32,
+    color: colors.light['text-primary'],
   },
 });
 
